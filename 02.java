b60aa01c95fed0d0daf97bec31c8e557d9bae54d
pelinglmz <143254601+pelinglmz@users.noreply.github.com>
2023-08-27 11:22:48 +0300

    Java3

---
+++ 02.java
@@ -0,0 +1,44 @@
+package denemee11;
+
+public class denemee11icindeki {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		System.out.println("hello!");
+			
+		System.out.println(" Bugun java ," 
+		        + " html de öğrenmeyi dusunuyorum "
+				+ " Birde php ogrenirsem guzel olur " );
+		
+	System.out.println(100);
+	
+	System.out.print("merhaba\n Dunya\n ");
+	System.out.println("ben pelin");
+	
+	System.out.println(" merhaba"
+			+ " nasılsınız ");
+	
+	System.out.println(10);
+	System.out.println(10+20);
+	System.out.println(10 + "25");
+	
+	System.out.print("merhaba\nnaber\n");
+	
+	System.out.println("\tmerhaba \n Dünya");
+	
+	
+	System.out.print("merhaba\nDunya\n");
+	System.out.println("nasilsiniz");
+	
+	
+	
+	
+	System.out.println("merhaba \t dunya");
+	System.out.println("\t merhaba dunyaa");
+	
+	
+	}
+
+}
