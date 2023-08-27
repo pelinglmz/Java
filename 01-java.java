578705532ca86bec0efd16d0bc58cfacb123c44f
pelinglmz <143254601+pelinglmz@users.noreply.github.com>
2023-08-27 11:20:14 +0300

    Java2

---
+++ 01-java.java
@@ -0,0 +1,19 @@
+package deneme1;
+
+public class AnaMetod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!"); 
+		System.out.println(" nasilsin "
+				+ "sizlerle beraber java ogreniyoruz "
+						+ "bende cok merak ediyoum :)"); 
+		
+	System.out.println(12 + "17");
+	System.out.println(12 + 17);
+	System.out.println("nasılsınız");
+	
+	
+      }
+}
